55c26eb609ee27db257c7fcc77d758c59ab79e58
Tom Wilson <tom@hyper.io>
2022-12-22 13:10:14 -0500

    Revert "CoreConcepts: Path Manifests"

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -51,10 +51,6 @@ module.exports = {
         {
           text: 'Tags',
           link: '/concepts/tags'
-        },
-        {
-            text: 'Path Manifests',
-            link: '/concepts/manifests'
         }]
       },
       {
